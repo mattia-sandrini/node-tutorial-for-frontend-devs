31308919c384c470b6b657abeabd96503e724e48
Mattia Sandrini <sandrinimattia@hotmail.it>
2019-12-17 12:12:41 +0100

    Aggiunte la index page e l'array di device_list.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,52 @@ var express = require('express');
 //var cors = require('cors')
 var router = express.Router();
 
+
+var device_list = [
+    {
+        id: "ENGIETEST",
+        content: {
+            id: "c1",
+            type: "multiple-message",
+            timestamp_creation: 1576160838,       
+            timestamp_expiration: 1576680851, 
+            timestamp_last_update: 1576160947,     
+            bodies: [
+                "<p  style=\"font-size: 40px; font-family: 'Courier New'\">Questo è il primo messaggio di una serie di 3 messaggi.</p>",
+                "<p style=\"color: #E74C3C; font-size: 40px; font-family: 'Courier New'\">Questo è il secondo messaggio, che a differenza del primo possiede una <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>formattazione</i></b></span> del testo.</p>",
+                "<p style=\"font-size: 40px; font-family: 'Courier New'\">L'ultimo messaggio dimostra il funzionamento di **lampeggio** del testo.</p>"
+            ],
+            rolling_interval: 10 
+        }
+    },
+    {
+        id: "ENGIEPMVTEST",
+        content: {
+            id: "c2",
+            type: "single-message",
+            timestamp_creation: 1576160838,       
+            timestamp_expiration: 1996326632, 
+            timestamp_last_update: 1576160947,     
+            body: "<p style=\"color: #E74C3C; font-size: 25px; font-family: 'Courier New'\">Messaggio visualizzato su <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>**PMV**</i></b></span>!</p><img src='https://www.engie.it/ENGIEit-theme/img/new-logo-blu.png' height='40px' width='auto' style='margin-right: 10px;' />",
+        }
+    }
+];
+
+
+var pmv_id_list = ["ENGIEPMVTEST", "ENGIETEST"];
+
+
+function getDeviceIndex(id) {
+    for (let i = 0; i < device_list.length; i++) {
+        if (device_list[i].id == id) {
+            return i;
+        }
+    }
+    return null;
+}
+
+
+
 // Add headers
 router.use(function (req, res, next) {
     // Websites allowed to connect
@@ -32,7 +78,9 @@ router.get('/pmv/:pmvId/content', function(req, res, next) {
     var pmvId = req.params.pmvId;
     var body_msg = "-";
 
-    if (pmvId == "1") {
+    console.log(pmvId);
+
+    /*if (pmvId == "1") {
         body_msg = "<p style=\"color: red; font-family: 'Courier New'\">Hello **from** <span style=\"color: blue; font-family: sans-serif\"><b>Node.js</b></span>.</p>";
         res.send({ 
             id: "c1",
@@ -43,22 +91,30 @@ router.get('/pmv/:pmvId/content', function(req, res, next) {
             body: body_msg
         });
     }
-    else if (pmvId == "2") {
-        body_msg = "<p style=\"color: red; font-family: 'Courier New'\">Lorem ipsum <span style=\"color: blue; font-family: sans-serif\"><b>dolor</b></span> sit amet.</p>";
+    else if (pmvId == "ENGIEPMVTEST") {
+        //body_msg = "<pstyle=\"color: red; font-family: 'Courier New'\">Lorem ipsum **dolor** sit amet.</p> <p style=\"color: red; font-family: 'Courier New'\">Lorem **ipsum** <span style=\"color: blue; font-family: sans-serif\"><b>dolor</b></span> sit amet.</p>";
         
+        body_msg = "<p style=\"color: #E74C3C; font-size: 25px; font-family: 'Courier New'\">Messaggio visualizzato su <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>**PMV**</i></b></span>!</p><img src='https://www.engie.it/ENGIEit-theme/img/new-logo-blu.png' height='40px' width='auto' style='margin-right: 10px;' />";
+
         res.send({ 
             id: "c2",
             type: "single-message",
             timestamp_creation: 1576160838,       
-            timestamp_expiration: 1576180851, 
+            timestamp_expiration: 1996326632, 
             timestamp_last_update: 1576160947,     
             body: body_msg
         });
     }
-    else if (pmvId == "3") {
-        body_msg_1 = "<p style=\"color: red; font-family: 'Courier New'\">Hello <span style=\"color: blue; font-family: sans-serif\"><b>**World**</b></span>.</p>";
-        body_msg_2 = "<p style=\"color: blue; font-family: 'Courier New'\">Lorem **ipsum** <span style=\"color: blue; font-family: sans-serif\"><i>dolor</i></span> **sit <b>amet</b>**.</p>";
-        body_msg_3 = "<p style=\"color: red; font-family: 'Courier New'\">**Lorem ipsum <span style=\"color: blue; font-family: sans-serif\"><b>dolor</b></span> sit amet**.</p>";
+    else if (pmvId == "ENGIETEST") {
+        //body_msg_1 = "<p style=\"color: red; font-family: 'Courier New'\">Hello <span style=\"color: blue; font-family: sans-serif\"><b>**World**</b></span>.</p>";
+        //body_msg_2 = "<p style=\"color: blue; font-family: 'Courier New'\">Lorem **ipsum** <span style=\"color: blue; font-family: sans-serif\"><i>dolor</i></span> **sit <b>amet</b>**.</p>";
+        //body_msg_3 = "<p style=\"color: red; font-family: 'Courier New'\">**Lorem ipsum <span style=\"color: blue; font-family: sans-serif\"><b>dolor</b></span> sit amet**.</p>";
+        
+        body_msg_1 = "<p  style=\"font-size: 40px; font-family: 'Courier New'\">Questo è il primo messaggio di una serie di 3 messaggi.</p>";
+        body_msg_2 = "<p style=\"color: #E74C3C; font-size: 40px; font-family: 'Courier New'\">Questo è il secondo messaggio, che a differenza del primo possiede una <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>formattazione</i></b></span> del testo.</p>";
+        body_msg_3 = "<p style=\"font-size: 40px; font-family: 'Courier New'\">L'ultimo messaggio dimostra il funzionamento di **lampeggio** del testo.</p>";
+
+        //res.send(device_list[0].content);
         res.send({ 
             id: "c3",
             type: "multiple-message",
@@ -68,11 +124,22 @@ router.get('/pmv/:pmvId/content', function(req, res, next) {
             bodies: [body_msg_1, body_msg_2, body_msg_3],
             rolling_interval: 10     // Secondi di visualizzazione di ogni messaggio 
         });
-    }
+    }*/
+
+    var requested_device = getDeviceIndex(pmvId);
+    /*device_list.forEach(device => {
+        if (device.id == pmvId) {
+            requested_device = device;
+            console.log(requested_device);
+            break;
+        }
+    });*/
+
+    if (requested_device != null) {
+        res.send(device_list[requested_device].content);
+    } 
     else {
-        res.send({ 
-            error: "Id \""+pmvId+"\" inesistente!"
-        });
+        res.status(404).send({error: "Id \""+pmvId+"\" inesistente!"});
     }
 });
   
@@ -88,7 +155,7 @@ router.put('/pmv/:pmvId/content/:contentId', function(req, res) {
     //var error = req.body.error;
     var timestamp = req.body.timestamp;
 
-    if (["1", "2", "3"].includes(pmvId)) {
+    if (pmv_id_list.includes(pmvId)) {
         if (req.body.hasOwnProperty("error")) {
 
             // TODO: trattare la presenza dell'errore
@@ -113,7 +180,6 @@ router.put('/pmv/:pmvId/content/:contentId', function(req, res) {
         }
     }
     else {
-        res.send({error: "Id \""+pmvId+"\" inesistente!"});
         res.status(404).send({error: "Id \""+pmvId+"\" inesistente!"});
     }
 });
@@ -126,7 +192,13 @@ router.put('/pmv/:pmvId/status', function(req, res) {
     var displayedContentId = req.body.displayed_content;
     var timestamp = req.body.timestamp;
 
-    if (["1", "2", "3"].includes(pmvId)) {
+
+    var requested_device = getDeviceIndex(pmvId);
+
+    if (requested_device != null) {
+        device_list[requested_device].status = status;
+        device_list[requested_device].displayed_content = displayedContentId;
+
         res.send({success: "Stato aggiornato con successo.", 
             // TODO: eliminare le righe sottostanti (utile solo in fase di test)
             status: status, 
@@ -135,8 +207,20 @@ router.put('/pmv/:pmvId/status', function(req, res) {
         });
     }
     else {
-        res.send({error: "Id \""+pmvId+"\" inesistente!"});
+        res.status(404).send({error: "Id \""+pmvId+"\" inesistente!"});
+    } 
+
+    /*if (pmv_id_list.includes(pmvId)) {
+        res.send({success: "Stato aggiornato con successo.", 
+            // TODO: eliminare le righe sottostanti (utile solo in fase di test)
+            status: status, 
+            displayed_content: displayedContentId,
+            timestamp: timestamp
+        });
     }
+    else {
+        res.status(404).send({error: "Id \""+pmvId+"\" inesistente!"});
+    }*/
 });
 
 
