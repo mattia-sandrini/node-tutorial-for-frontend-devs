63970f404a5ff733ca8c3a68c84100a78312fa64
Mattia Sandrini <sandrinimattia@hotmail.it>
2019-12-18 01:31:59 +0100

    Funziona anche lo stato

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// New Code
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+//var monk = require('monk');
+//var db = monk('localhost:27017/nodetest1');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -24,13 +23,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
+/*app.use(function(req,res,next){
     req.db = db;
     next();
-});
+});*/
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,5 +46,36 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+app.locals.device_list = [
+  {
+      id: "ENGIETEST",
+      content: {
+          id: "c1",
+          type: "multiple-message",
+          timestamp_creation: 1576160838,       
+          timestamp_expiration: 1576954800, 
+          timestamp_last_update: 1576160947,     
+          bodies: [
+              "<p  style=\"font-size: 40px; font-family: 'Courier New'\">Questo è il primo messaggio di una serie di 3 messaggi.</p>",
+              "<p style=\"color: #E74C3C; font-size: 40px; font-family: 'Courier New'\">Questo è il secondo messaggio, che a differenza del primo possiede una <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>formattazione</i></b></span> del testo.</p>",
+              "<p style=\"font-size: 40px; font-family: 'Courier New'\">L'ultimo messaggio dimostra il funzionamento di **lampeggio** del testo.</p>"
+          ],
+          rolling_interval: 10 
+      }
+  },
+  {
+      id: "ENGIEPMVTEST",
+      content: {
+          id: "c2",
+          type: "single-message",
+          timestamp_creation: 1576160838,       
+          timestamp_expiration: 1576954800, 
+          timestamp_last_update: 1576160947,     
+          body: "<p style=\"color: #E74C3C; font-size: 25px; font-family: 'Courier New'\">Messaggio visualizzato su <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>**PMV**</i></b></span>!</p><img src='https://www.engie.it/ENGIEit-theme/img/new-logo-blu.png' height='40px' width='auto' style='margin-right: 10px;' />",
+      }
+  }
+];
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 
-var device_list = [
-    {
-        id: "ENGIETEST",
-        content: {
-            id: "c1",
-            type: "multiple-message",
-            timestamp_creation: 1576160838,       
-            timestamp_expiration: 1576954800, 
-            timestamp_last_update: 1576160947,     
-            bodies: [
-                "<p  style=\"font-size: 40px; font-family: 'Courier New'\">Questo è il primo messaggio di una serie di 3 messaggi.</p>",
-                "<p style=\"color: #E74C3C; font-size: 40px; font-family: 'Courier New'\">Questo è il secondo messaggio, che a differenza del primo possiede una <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>formattazione</i></b></span> del testo.</p>",
-                "<p style=\"font-size: 40px; font-family: 'Courier New'\">L'ultimo messaggio dimostra il funzionamento di **lampeggio** del testo.</p>"
-            ],
-            rolling_interval: 10 
-        }
-    },
-    {
-        id: "ENGIEPMVTEST",
-        content: {
-            id: "c2",
-            type: "single-message",
-            timestamp_creation: 1576160838,       
-            timestamp_expiration: 1576954800, 
-            timestamp_last_update: 1576160947,     
-            body: "<p style=\"color: #E74C3C; font-size: 25px; font-family: 'Courier New'\">Messaggio visualizzato su <span style=\"color: #2980B9; font-family: sans-serif\"><b><i>**PMV**</i></b></span>!</p><img src='https://www.engie.it/ENGIEit-theme/img/new-logo-blu.png' height='40px' width='auto' style='margin-right: 10px;' />",
-        }
-    }
-];
+
 
 
 var pmv_id_list = ["ENGIEPMVTEST", "ENGIETEST"];
 
 
-function getDeviceIndex(id) {
+function getDeviceIndex(device_list, id) {
     for (let i = 0; i < device_list.length; i++) {
         if (device_list[i].id == id) {
             return i;
@@ -50,6 +22,8 @@ function getDeviceIndex(id) {
 
 // Add headers
 router.use(function (req, res, next) {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+
     // Websites allowed to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -69,12 +43,15 @@ router.use(function (req, res, next) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var device_list = req.app.locals.device_list;
   res.render('index', { title: 'Test Engie', device_list: device_list });
 });
 
 
 /* Prevede la discriminazione dei contenuti in base all'id del pmv specificato nella URL */
 router.get('/pmv/:pmvId/content', function(req, res, next) {
+    var device_list = req.app.locals.device_list;
+
     var pmvId = req.params.pmvId;
     var body_msg = "-";
 
@@ -126,7 +103,7 @@ router.get('/pmv/:pmvId/content', function(req, res, next) {
         });
     }*/
 
-    var requested_device = getDeviceIndex(pmvId);
+    var requested_device = getDeviceIndex(device_list, pmvId);
     /*device_list.forEach(device => {
         if (device.id == pmvId) {
             requested_device = device;
@@ -148,13 +125,15 @@ router.get('/pmv/:pmvId/content', function(req, res, next) {
 // Il PMV aggiorna lo stato del contenuto visualizzato come feedback alla richiesta di ottenere il contenuto da visualizzare
 
 router.put('/pmv/:pmvId/content/:contentId', function(req, res) {
+    var device_list = req.app.locals.device_list;
+
     var pmvId = req.params.pmvId;
     var contentId = req.params.contentId;
 
     var status = req.body.status;        // "displayed|expired|rejected"
     var timestamp = req.body.timestamp;
 
-    var requested_device = getDeviceIndex(pmvId);
+    var requested_device = getDeviceIndex(device_list, pmvId);
 
     if (requested_device != null) {
         if (req.body.hasOwnProperty("error")) {
@@ -164,9 +143,10 @@ router.put('/pmv/:pmvId/content/:contentId', function(req, res) {
             // TODO: trattare la presenza dell'errore
 
             res.send({success: "Feedback ricevuto.", 
+                device: device_list[requested_device],
                 // TODO: eliminare le righe sottostanti (utile solo in fase di test)
-                content_id: contentId,
-                status: status, 
+                //content_id: contentId,
+                //status: status, 
                 error: req.body.error,
                 timestamp: timestamp
             });
@@ -177,9 +157,10 @@ router.put('/pmv/:pmvId/content/:contentId', function(req, res) {
             delete(device_list[requested_device].content.error);
 
             res.send({success: "Feedback ricevuto.", 
+                device: device_list[requested_device],
                 // TODO: eliminare le righe sottostanti (utile solo in fase di test)
-                content_id: contentId,
-                status: status, 
+                //content_id: contentId,
+                //status: status, 
                 timestamp: timestamp
             });
         }
@@ -191,6 +172,8 @@ router.put('/pmv/:pmvId/content/:contentId', function(req, res) {
 
 
 router.put('/pmv/:pmvId/status', function(req, res) {
+    var device_list = req.app.locals.device_list;
+
     var pmvId = req.params.pmvId;
 
     var status = req.body.status;    // "active/idle/stale" --> Il PMV può essere in modalità di visualizzazione del messaggio, o in stallo
@@ -198,7 +181,7 @@ router.put('/pmv/:pmvId/status', function(req, res) {
     var timestamp = req.body.timestamp;
 
 
-    var requested_device = getDeviceIndex(pmvId);
+    var requested_device = getDeviceIndex(device_list, pmvId);
 
     if (requested_device != null) {
         device_list[requested_device].status = status;
@@ -231,11 +214,30 @@ router.put('/pmv/:pmvId/status', function(req, res) {
 
 
 
+
+
+
+router.get('/pmv/:pmvId', function(req, res) {
+    var device_list = req.app.locals.device_list;
+
+    var pmvId = req.params.pmvId;
+    var requested_device = getDeviceIndex(device_list, pmvId);
+
+    if (requested_device != null) {
+        res.send(device_list[requested_device]);
+    } 
+    else {
+        res.status(404).send({error: "Id \""+pmvId+"\" inesistente!"});
+    }
+});
+
 router.put('/pmv/:pmvId', function(req, res) {
+    var device_list = req.app.locals.device_list;
+    
     var pmvId = req.params.pmvId;
 
     var content = req.body.content;   
-    var requested_device = getDeviceIndex(pmvId);
+    var requested_device = getDeviceIndex(device_list, pmvId);
 
     if (requested_device != null) {
         device_list[requested_device].content = content;
@@ -247,76 +249,5 @@ router.put('/pmv/:pmvId', function(req, res) {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
-});
-
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-    });
-});
-
-/* GET New User page. */
-router.get('/newuser', function(req, res) {
-    res.render('newuser', { title: 'Add New User' });
-});
-
-/* POST to Add User Service */
-router.post('/adduser', function(req, res) {
-
-    // Set our internal DB variable
-    var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-
-    // Set our collection
-    var collection = db.get('usercollection');
-
-    // Submit to the DB
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // And forward to success page
-            res.redirect("userlist");
-        }
-    });
-});
 
 module.exports = router;
